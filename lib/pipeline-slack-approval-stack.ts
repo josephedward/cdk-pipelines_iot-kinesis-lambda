ca55665195286f8c9209daa752e1ddb9461cad46
JoeV <15126922+josephedward@users.noreply.github.com>
2022-06-18 00:43:20 -0400

    Update pipeline-slack-approval-stack.ts

--- lib/pipeline-slack-approval-stack.ts
+++ lib/pipeline-slack-approval-stack.ts
@@ -4,7 +4,6 @@ import {
   CodeCommitSourceAction,
   GitHubSourceAction,
   CodeBuildAction,
-  
 } from "@aws-cdk/aws-codepipeline-actions";
 import { PipelineProject } from "@aws-cdk/aws-codebuild";
 
@@ -14,29 +13,26 @@ import {
 } from "@cloudcomponents/cdk-codepipeline-slack";
 import { SecretValue } from "@aws-cdk/core";
 
-   
-import { RestApi } from '@aws-cdk/aws-apigateway';
+import { RestApi } from "@aws-cdk/aws-apigateway";
 
 export class CodePipelineSlackApprovalStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-
     // const api = new RestApi(this, 'github-webhook');
     // api.root.addMethod('POST');
-
 
     const sourceArtifact = new Artifact();
 
     const sourceAction = new GitHubSourceAction({
-        actionName: 'Github_Source',
-        output: sourceArtifact,
-        owner: 'josephedward',
-        repo: 'example_cdk',
-        branch: 'main',
-        oauthToken: SecretValue.secretsManager('github-token'),
-        variablesNamespace: 'MyNamespace', // optional - by default, a name will be generated for you
-      });
+      actionName: "Github_Source",
+      output: sourceArtifact,
+      owner: "josephedward",
+      repo: "example_cdk",
+      branch: "main",
+      oauthToken: SecretValue.secretsManager("github-token"),
+      variablesNamespace: "MyNamespace", // optional - by default, a name will be generated for you
+    });
 
     const project = new PipelineProject(this, "MyProject");
 
@@ -46,11 +42,12 @@ export class CodePipelineSlackApprovalStack extends Stack {
       input: sourceArtifact,
     });
 
-    const slackBotToken = 'xoxb-3621546930117-3637169135025-QxnyuOjuFMXi7z4mxcEPLpVE'
+    // get a new, enabled token
+    const slackBotToken = "";
     //  process.env.SLACK_BOT_TOKEN as string;
-    const slackSigningSecret = '4f75de4d38931c04ef75317324f6100f'
+    const slackSigningSecret = "";
     // process.env.SLACK_SIGNING_SECRET as string;
-    const slackChannel = 'pipeline-approval-flow'
+    const slackChannel = "pipeline-approval-flow";
     // process.env.SLACK_CHANNEL_NAME as string;
 
     const approvalAction = new SlackApprovalAction({
